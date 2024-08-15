a6e465410e8fa43d56f32ad226769a70725ee7f9
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2024-08-15 20:06:58 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,16 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word_test, expected",
+    [
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True),
+    ]
+)
+def test_case(word_test: str, expected: bool) -> None:
+    assert is_isogram(word_test) == expected
